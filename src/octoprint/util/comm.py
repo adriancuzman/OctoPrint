4ff0b42ea8e32e2086e6c168fbd1fcf6b8ee544c
Adrian Cuzman <adriancuzman@gmail.com>
2018-02-27 11:31:33 +0200

    if resendCount > 50 do a new resend

--- src/octoprint/util/comm.py
+++ src/octoprint/util/comm.py
@@ -2173,7 +2173,8 @@ class MachineCom(object):
 				self._logger.info("Ignoring resend request for line %d, that still originates from lines we sent "
 				                   "before we got the first resend request" % lineToResend)
 				self._currentResendCount += 1
-				return True
+				if self._currentResendCount < 50:
+					return True
 
 			# If we ignore resend repetitions (Repetier firmware...), check if we
 			# need to do this now. If the same line number has been requested we
